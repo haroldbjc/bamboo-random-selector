f794f71ce5a3ace229896935863e1907905a99fa
haroldbjc <harold.bjc@gmail.com>
2025-01-29 13:43:07 +0800

    perf: use react compiler

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: "/brs",
   output: "export",
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default nextConfig;
